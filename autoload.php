ce4c444e86215dcca63537066fd5fa7ce6a7b1d1
Ivo Petkov <ivo@ivopetkov.com>
2016-07-03 17:54:21 +0300

    Added custom autoloader.

---
+++ autoload.php
@@ -0,0 +1,14 @@
+<?php
+
+$classes = array(
+    'IvoPetkov\HTML5DOMDocument' => __DIR__ . '/src/HTML5DOMDocument.php',
+    'IvoPetkov\HTML5DOMElement' => __DIR__ . '/src/HTML5DOMElement.php',
+    'IvoPetkov\HTML5DOMNodeList' => __DIR__ . '/src/HTML5DOMNodeList.php'
+);
+
+spl_autoload_register(function ($class) use ($classes) {
+    if (isset($classes[$class])) {
+        require $classes[$class];
+    }
+});
+
